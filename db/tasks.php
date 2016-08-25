11ae000df9adf8ded75487c86d66d5b14524511c
David Elliott <elliott2081@gmail.com>
2016-08-25 15:44:11 -0500

    updated version and adjusted cron to run twice a day

--- db/tasks.php
+++ db/tasks.php
@@ -4,9 +4,9 @@ $tasks = array(
         'classname' => 'block_etextbook\task\retrieve_etextbooks',                                                                            
         'blocking' => 0,                                                                                                            
         'minute' => '30',                                                                                                            
-        'hour' => '17',                                                                                                              
+        'hour' => '4,16',
         'day' => '*',                                                                                                               
         'dayofweek' => '0',                                                                                                         
-        'month' => '1,7'                                                                                                              
+        'month' => '*'
     )
 );
